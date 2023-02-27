f2abbfa97d8e8a64364498f8a0f8afcba2194039
Elvira Uzlova <e.uzlovaia@gmail.com>
2023-02-28 00:19:36 +0100

    Fixed mistakes.txt

--- test_for_google.py
+++ test_for_google.py
@@ -6,10 +6,9 @@ from selene import be, have
 @pytest.fixture(scope="session")
 def setup():
     browser.config.browser_name = "chrome"
-    browser.config.base_url = "https://www.google.com/ncr "
     browser.config.window_height = '768'
     browser.config.window_width = '1366'
-    browser.open('https://www.google.com')
+    browser.open('https://www.google.com/ncr')
 
 
 @pytest.fixture(scope="function")
@@ -18,12 +17,13 @@ def clear_search_field():
 
 
 def test_search_results_successful(setup, clear_search_field):
-    browser.element('button+button').click()  # эта строка закрывает GDPR поп-ап(нажимает Accept all)
+    # эта строка закрывает GDPR если он есть (нажимает Accept all)
+    browser.element('button+button').click()
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+    browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
 
 
 def test_search_results_unsuccessful(setup, clear_search_field):
     browser.element('[name="q"]').should(be.blank).type('j//nbn1231m==nm!!--a343n-').press_enter()
     browser.element('.card-section').should(
-        have.text('Na váš předmět vyhledávání - j//nbn1231m==nm!!--a343n- - nebyl nalezen žádný odkaz.'))
+        have.text('Your search - j//nbn1231m==nm!!--a343n- - did not match any documents.'))
